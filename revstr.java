3df8c145d63d6996ec8bad5e640be887a3e8ac8e
haneypatel <haneypatel7@gmail.com>
2023-09-28 00:05:27 +0530

    ad

---
+++ revstr.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class revstr
+{
+    public static void main(String args[])
+    {   
+        // Scanner sc=new Scanner(System.in);
+        // String name=sc.nextLine();
+        Stringbuilder sb=new Stringbuilder("haneypatel");
+        sb.append("isstudent");
+        System.out.println(sb);
+        
+    }
+
+}
