bb0fa745bd9fbc3b00a5e43d12384839700da474
haneypatel <haneypatel7@gmail.com>
2024-06-14 14:14:15 +0530

    b

---
+++ String/rev.java
@@ -0,0 +1,18 @@
+
+class rev {
+    public static void main(String[] args) {
+      String name="Haneypatel";
+      String name2="";
+      
+      for(int i=name.length()-1;i>=0;i--)
+      {
+          name2+=name.charAt(i);
+      }
+      //
+      
+      System.out.println(name2);
+    
+    
+        
+}
+}
